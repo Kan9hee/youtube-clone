3afd60b9d8c59dce300309a71c2d64fbaf8a582d
han <hcsy1219@gmail.com>
2023-01-28 19:13:59 +0900

    feat: Connect to azure

---
+++ src/main/java/com/clonetube/youtubeclone/service/VideoService.java
@@ -0,0 +1,12 @@
+package com.clonetube.youtubeclone.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class VideoService {
+    public void uploadVideo(MultipartFile multipartFile){
+        //Azure Blob에 파일 업로드 및 데이터베이스에 비디오 데이터 저장
+
+    }
+}
