1cd21c7223448c3322549d4b274c82eb9092e678
han <hcsy1219@gmail.com>
2023-02-14 00:09:22 +0900

    feat: Add like/dislike count system

--- backend/src/main/java/com/clonetube/youtubeclone/service/VideoService.java
+++ backend/src/main/java/com/clonetube/youtubeclone/service/VideoService.java
@@ -14,6 +14,7 @@ public class VideoService {
 
     private final BlobService blobService;
     private final VideoRepository videoRepository;
+    private final UserService userService;
 
     public UploadVideoResponse uploadVideo(MultipartFile multipartFile){
         //Azure Blob에 파일 업로드 및 데이터베이스에 비디오 데이터 저장
@@ -60,15 +61,77 @@ public class VideoService {
     public VideoDto getVideoDetails(String videoId) {
         Video savedVideo = getVideoById(videoId);
 
-        VideoDto videoDto = new VideoDto();
-        videoDto.setVideoUrl(savedVideo.getVideoUrl());
-        videoDto.setThumbnailUrl(savedVideo.getThumbnailUrl());
-        videoDto.setId(savedVideo.getId());
-        videoDto.setTitle(savedVideo.getTitle());
-        videoDto.setDescription(savedVideo.getDescription());
-        videoDto.setTags(savedVideo.getTags());
-        videoDto.setVideoStatus(savedVideo.getVideoStatus());
+        increaseVideoCount(savedVideo);
+        userService.addVideoToHistory(videoId);
 
+        return mapToVideoDto(savedVideo);
+    }
+
+    public VideoDto likeVideo(String videoId) {
+        Video videoById=getVideoById(videoId);
+        //좋아요 증가
+        //이미 좋아요를 눌렀다면, 좋아요 감소
+        //이미 싫어요를 눌렀다면, 좋아요 증가, 싫어요 감소
+
+        if(userService.ifLikedVideo(videoId)){
+            videoById.decrementLikes();
+            userService.removeFromLikedVideos(videoId);
+        }else if(userService.ifDisLikedVideo(videoId)){
+            videoById.decrementDisLikes();
+            userService.removeFromDisLikedVideos(videoId);
+            videoById.incrementLikes();
+            userService.addToLikedVideos(videoId);
+        }else {
+            videoById.incrementLikes();
+            userService.addToLikedVideos(videoId);
+        }
+
+        videoRepository.save(videoById);
+
+        return mapToVideoDto(videoById);
+    }
+
+    private void increaseVideoCount(Video savedVideo){
+        savedVideo.incrementViewCount();
+        videoRepository.save(savedVideo);
+    }
+
+    public VideoDto disLikeVideo(String videoId) {
+        Video videoById=getVideoById(videoId);
+        //싫어요 증가
+        //이미 싫어요를 눌렀다면, 싫어요 감소
+        //이미 좋아요를 눌렀다면, 싫어요 증가, 좋아요 감소
+
+        if(userService.ifLikedVideo(videoId)){
+            videoById.decrementDisLikes();
+            userService.removeFromDisLikedVideos(videoId);
+        }else if(userService.ifLikedVideo(videoId)){
+            videoById.decrementLikes();
+            userService.removeFromLikedVideos(videoId);
+            videoById.incrementDisLikes();
+            userService.addToDisLikedVideos(videoId);
+        }else {
+            videoById.incrementLikes();
+            userService.addToDisLikedVideos(videoId);
+        }
+
+        videoRepository.save(videoById);
+
+        return mapToVideoDto(videoById);
+    }
+
+    private VideoDto mapToVideoDto(Video videoById) {
+        VideoDto videoDto = new VideoDto();
+        videoDto.setVideoUrl(videoById.getVideoUrl());
+        videoDto.setThumbnailUrl(videoById.getThumbnailUrl());
+        videoDto.setId(videoById.getId());
+        videoDto.setTitle(videoById.getTitle());
+        videoDto.setDescription(videoById.getDescription());
+        videoDto.setTags(videoById.getTags());
+        videoDto.setVideoStatus(videoById.getVideoStatus());
+        videoDto.setLikeCount(videoById.getLikes().get());
+        videoDto.setDislikeCount(videoById.getDisLikes().get());
+        videoDto.setViewCount(videoById.getViewCount().get());
         return videoDto;
     }
 }
